ae54be3ce83d8e23cb1551ed0c7b61d7a3c21597
Ilya Lebedev <bacherko@gmail.com>
2018-10-05 15:12:13 +0300

    fix phone format #37

--- utils/phone.js
+++ utils/phone.js
@@ -7,9 +7,11 @@ export function formatPhone(phone) {
 
     const begin = groups[1];
     let start;
+    let mid;
 
     if (/^[17]/.test(begin)) {
-        start = `${begin[0]} (${begin.substr(1)}) `;
+        start = begin[0];
+        mid = begin.substr(1);
     } else if (
         /^2[1234569]/.test(begin) ||
         /^3[578]/.test(begin) ||
@@ -19,10 +21,16 @@ export function formatPhone(phone) {
         /^8[0578]/.test(begin) ||
         /^9[679]/.test(begin)
     ) {
-        start = `${begin.substr(0, 3)} (${begin.substr(3)}) `;
+        start = begin.substr(0, 3);
+        mid = begin.substr(3);
     } else {
-        start = `${begin.substr(0, 2)} (${begin.substr(2)}) `;
+        start = begin.substr(0, 2);
+        mid = begin.substr(2);
     }
 
-    return `${start} ${groups[2]}-${groups[3]}-${groups[4]}`;
+    if (mid.length > 1) {
+        mid = `(${mid}) `;
+    }
+
+    return `${start} ${mid}${groups[2]}-${groups[3]}-${groups[4]}`;
 }
