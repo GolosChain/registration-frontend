6f968f0dfa9037091c7a7d61129fc739b6cb6033
Ilya Lebedev <bacherko@gmail.com>
2018-10-05 19:15:30 +0300

    improve 3 step

--- components/Common.js
+++ components/Common.js
@@ -44,6 +44,8 @@ export const Button = styled.button.attrs({ type: 'button' })`
     cursor: pointer;
 
     &[disabled] {
+        cursor: default;
+        cursor: not-allowed;
         background: #89cbff;
     }
 `;
