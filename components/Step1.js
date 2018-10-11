1059492629fc826170d2192e86a630a230808d5f
Ilya Lebedev <bacherko@gmail.com>
2018-10-11 15:47:45 +0300

    33 fix 2 (#56)
    * fix checking #33
    
    * fix placeholder #32

--- components/Step1.js
+++ components/Step1.js
@@ -267,7 +267,7 @@ export default class Step1 extends PureComponent {
 
         const accountName = value.toLowerCase().replace(/[^a-z0-9.-]+/g, '');
 
-        if (value !== accountName) {
+        if (value.toLowerCase() !== accountName) {
             this.setState({
                 accountNameTempErrorText: 'step1.rules.containsOnly',
             });
@@ -346,7 +346,7 @@ export default class Step1 extends PureComponent {
             accountNameErrorText: errorText,
         };
 
-        if (error) {
+        if (errorText) {
             newState.accountNameTempErrorText = null;
         }
 
